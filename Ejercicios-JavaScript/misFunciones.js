aab41e7bef550f033006156e59c872fa5d160efc
manuel frias <friasmanuel42@gmail.com>
2023-05-28 11:42:34 -0300

    operaciones

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -56,3 +56,31 @@ function mostrar_ocultar(valor){
         document.getElementById("divMO").style.display = 'none';
     }
 }
+
+function suma(){
+    var n1, n2;
+    n1 = Number(document.getElementsByName("sum_num1")[0].value);
+    n2 =Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].value = n1 + n2;
+}
+
+function resta(){
+    var n1, n2;
+    n1 = Number(document.getElementsByName("res_num1")[0].value);
+    n2 =Number(document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].value = n1 - n2;
+}
+
+function multi(){
+    var n1, n2;
+    n1 = Number(document.getElementsByName("mul_num1")[0].value);
+    n2 =Number(document.getElementsByName("mul_num2")[0].value);
+    document.getElementsByName("mul_total")[0].value = n1 * n2;
+}
+
+function div(){
+    var n1, n2;
+    n1 = Number(document.getElementsByName("div_num1")[0].value);
+    n2 =Number(document.getElementsByName("div_num2")[0].value);
+    document.getElementsByName("div_total")[0].value = n1 / n2;
+}
